6267e81738656f82462f745b826eb94f2f85ac77
ismayank <mayanksinghchakulia@gmail.com>
2022-11-06 18:51:10 +0530

    signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from 'react';
+import './Signup.css';
+import {}from "react-router";
+import {Link } from "react-router-dom";
 
 function Signup() {
-  return (
-    <div>Signup</div>
-  )
+    return (
+        
+        <div className="main">
+        <div className="sign">
+        <h1>Signup</h1>
+    </div><div>
+            <main className="box">
+                <form>
+                    <div className="inputBox">
+                        <label htmlFor="Email">Email</label>
+                        <input type="text" name="Email" id="Email" required />
+                    </div>
+                    <div className="inputBox">
+                        <label htmlFor="Name">Name</label>
+                        <input type="text" name="Name" id="Name"
+                            required />
+                    </div>
+                    <div className="inputBox">
+                        <label htmlFor="Password"> Password</label>
+                        <input type="password" name="Password" id="Password"
+
+                            required />
+                    </div>
+                    <button type="submit" name="" style={{ alignContent: "center" }}>Signup</button>
+                    
+                    <Link className="login-link" to="/login" style={{ float: "center" }}><span className="login-span">Already signed up? Login now!</span></Link>
+
+                </form>
+
+            </main>
+        </div></div>
+        
+    );
 }
 
-export default Signup
+export default Signup;
