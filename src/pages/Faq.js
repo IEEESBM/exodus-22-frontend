4c7874dabf17ba92b3f82d4bd1047226b75b2560
Piyush Mohapatra <piyush.ofcl@gmail.com>
2022-11-07 22:48:17 +0530

    FAQ Page Added
    Co-Authored-By: Sweta <93282010+Sweta6420@users.noreply.github.com>

--- src/pages/Faq.js
+++ src/pages/Faq.js
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import data from './data';
+import SingleQuestion from './Question';
+import "./faq.css";
+import Group7 from '../Images/Group7.svg'
+
 
 function Faq() {
+  const [questions, setQuestions] = useState(data)
   return (
-    <div>Faq</div>
+    <div className='faq-page'>
+      <div class="row" id="main-row">
+        <div className="column" id="title-col">
+          <div className="title">
+            <p>Frequently Asked Questions</p>
+          </div>
+          <div className="faq-content">
+            <img className="bg-vector" src={Group7}></img>
+          </div>
+        </div>
+        <div className="column" id="info-col">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question} />
+            })
+          }
+        </div>
+      </div>
+    </div>
   )
 }
 
